911ec90dae1dfcdc9070b0ee362f8541fbfac571
juliothecesar10 <82733183+juliothecesar10@users.noreply.github.com>
2025-10-23 07:10:21 -0300

    Add files via upload

---
+++ exemplos/ChessBoard.java
@@ -0,0 +1,58 @@
+package sef.module3.sample;
+
+public class ChessBoard {
+	public static void main(String[] args) {
+		
+		String[][] board = { 
+				{"R","N","B","Q","K","B","N","R"},
+				{"P","P","P","P","P","P","P","P"},
+				{" "," "," "," "," "," "," "," "},
+				{" "," "," "," "," "," "," "," "},
+				{" "," "," "," "," "," "," "," "},
+				{" "," "," "," "," "," "," "," "},
+				{"p","p","p","p","p","p","p","p"},
+				{"r","n","b","q","k","b","n","r"}};
+
+		 for (int i = 0; i < board.length; i++) {
+		        for (int j = 0; j < board[i].length; j++) {
+		        	System.out.print(board[i][j]+"  ");
+		        }
+		        System.out.println("");
+		    }
+		 
+		 //Mover Cavalo
+		 System.out.println("");
+		 board[5][5] = board[7][6];
+		 board[7][6] = "";
+
+		 for (int i = 0; i < board.length; i++) {
+		        for (int j = 0; j < board[i].length; j++) {
+		        	System.out.print(board[i][j]+"  ");
+		        }
+		        System.out.println("");
+		    }
+		 
+		 // Mover Peao
+		 System.out.println("");
+		 board[4][2] = board[6][2];
+		 board[6][2] = " ";
+		 
+		 for (int i = 0; i < board.length; i++) {
+		        for (int j = 0; j < board[i].length; j++) {
+		        	System.out.print(board[i][j]+"  ");
+		        }
+		        System.out.println("");
+		    }
+//		 imprimirBoard(board);
+	}
+	
+	// --------------Impress�o do Tabuleiro---------------------------------------
+	public void imprimirBoard(String[][] board) {
+		 for (int i = 0; i < board.length; i++) {
+		        for (int j = 0; j < board[i].length; j++) {
+		        	System.out.print(board[i][j]+"  ");
+		        }
+		        System.out.println("");
+		    }
+	}
+}
